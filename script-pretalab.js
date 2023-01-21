9b17f39d310d9c0d3d8d3bd1f7ae40f458c43c3b
DevFaBGirl <fabgirldev@gmail.com>
2023-01-21 10:40:58 -0300

    primeiro commit da oficina de Github

---
+++ script-pretalab.js
@@ -0,0 +1,6 @@
+//alert("Olá mulheres maravilhosas!");
+//prompt ("Disserte sobre um momento inesquecível");
+
+alert ("Olá, querida!");
+const nomeUsuaria = prompt('Me chamo Fab, qual seu nome?');
+alert(`Bem vinda ao meu primeiro programa ${nomeUsuaria} :D`);
